f5ce4900ac56e9461eab365aeaba2ee435475d85
Joozd <joozd@ius.org>
2020-09-10 12:04:40 +0200

    changed backup character (replacing ';') for csv export from '~' to '|'

--- app/src/main/java/nl/joozd/logbookapp/data/export/FlightsRepositoryExporter.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/export/FlightsRepositoryExporter.kt
@@ -80,7 +80,7 @@ class FlightsRepositoryExporter(val flightRepository: FlightRepository): Corouti
                 // DELETEFLAG,
                 // timeStamp,
                 Base64.encodeToString(signature.toByteArray(Charsets.UTF_8), Base64.NO_WRAP)
-            ).joinToString(";") { it.replace(';', '~') }
+            ).joinToString(";") { it.replace(';', '|') }
 
         }
     }
@@ -112,7 +112,7 @@ class FlightsRepositoryExporter(val flightRepository: FlightRepository): Corouti
 
 
 
-        private fun csvFlightToBasicFlightv4(csvFlight: String): BasicFlight_version4 = csvFlight.also{Log.d("XXXXXXXXXXXX", it)}.split(';').map{ it.replace('~', ';')}.let { v->
+        private fun csvFlightToBasicFlightv4(csvFlight: String): BasicFlight_version4 = csvFlight.also{Log.d("XXXXXXXXXXXX", it)}.split(';').map{ it.replace('|', ';')}.let { v->
             require(BasicFlight_version4.VERSION.version == 4)
             BasicFlight_version4(
                 flightID = -1,
@@ -152,7 +152,7 @@ class FlightsRepositoryExporter(val flightRepository: FlightRepository): Corouti
             )
         }
 
-        private fun csvFlightToBasicFlightv5(csvFlight: String): BasicFlight = csvFlight.also{Log.d("XXXXXXXXXXXX", it)}.split(';').map{ it.replace('~', ';')}.let { v->
+        private fun csvFlightToBasicFlightv5(csvFlight: String): BasicFlight = csvFlight.also{Log.d("XXXXXXXXXXXX", it)}.split(';').map{ it.replace('|', ';')}.let { v->
             require(BasicFlight.VERSION.version == 5)
             BasicFlight(
                 flightID = -1,

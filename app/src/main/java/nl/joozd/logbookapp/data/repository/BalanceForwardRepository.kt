e41d62eb55047dcace6bfde37669adbb97c2ff16
joozd <euroshopper@gmail.com>
2021-07-29 10:54:51 +0200

    Documentation

--- app/src/main/java/nl/joozd/logbookapp/data/repository/BalanceForwardRepository.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/repository/BalanceForwardRepository.kt
@@ -25,14 +25,20 @@ import nl.joozd.logbookapp.data.dataclasses.BalanceForward
 import nl.joozd.logbookapp.data.room.JoozdlogDatabase
 import nl.joozd.logbookapp.data.room.dao.BalanceForwardDao
 
-//TODO documentation!
 class BalanceForwardRepository(private val balanceforwardDao: BalanceForwardDao, private val dispatcher: CoroutineDispatcher = Dispatchers.IO): CoroutineScope by MainScope()  {
 
     /**********************************************************************************************
      * Private parts
      **********************************************************************************************/
 
+    /**
+     * Save a [BalanceForward] to DB
+     */
     private fun saveToDisk(bf: BalanceForward) = launch(dispatcher + NonCancellable) { balanceforwardDao.save(bf) }
+
+    /**
+     * Deleted BalanceForward for undo purposes
+     */
     private var deletedBF: BalanceForward? = null
 
     /**********************************************************************************************
@@ -41,10 +47,15 @@ class BalanceForwardRepository(private val balanceforwardDao: BalanceForwardDao,
 
     suspend fun getAll(): List<BalanceForward> = balanceforwardDao.requestAll()
 
+    /**
+     * Get LiveData reference to all BalanceForwards
+     */
     fun getLive() = balanceforwardDao.requestLiveBalancesForward()
 
     /**
+     * Save a BalanceForward to DB
      * A BalanceForward with ID -1 gets a new one autoAssigned
+     * @param bf: The BalanceForward to save
      */
     fun save (bf: BalanceForward) {
         if (bf.id != -1) saveToDisk(bf)
@@ -56,15 +67,26 @@ class BalanceForwardRepository(private val balanceforwardDao: BalanceForwardDao,
         }
     }
 
+    /**
+     * Save a list of BalanceForwards
+     * @param bff: Balance Forwards
+     */
     fun save (bff: List<BalanceForward>) = launch(dispatcher + NonCancellable) {
         balanceforwardDao.save(*bff.toTypedArray())
     }
 
+    /**
+     * Delete a specific BalanceForward from DB
+     */
     fun delete(bf: BalanceForward) = launch(dispatcher + NonCancellable) {
         deletedBF = bf
         balanceforwardDao.delete(bf)
     }
 
+    /**
+     * Undo most recent deletion of a balance forward
+     * NOTE only the most decent deletion can be undeleted.
+     */
     fun undelete(): Boolean {
         deletedBF?.let{
             save(it)

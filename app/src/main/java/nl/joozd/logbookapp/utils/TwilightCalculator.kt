04d2efc079f7c76dc123320dab822536cebeff91
joozd <euroshopper@gmail.com>
2021-09-27 11:53:19 +0200

    Removed debug logging

--- app/src/main/java/nl/joozd/logbookapp/utils/TwilightCalculator.kt
+++ app/src/main/java/nl/joozd/logbookapp/utils/TwilightCalculator.kt
@@ -45,14 +45,10 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
             axis *cos(
                 j *day))+ twilightAngle /cos(lat.toRadians())
         val lengthOfDay = acos(1-n)/Math.PI
-        // Log.d("latLon:", "$lat, $lon")
-        // Log.d("lengthOfDay:", (lengthOfDay*24).toString())
         val utcOffset= ((lon/15)*60).toLong()
         val sunriseInZ = LocalDateTime.of(date,LocalTime.of(12,0)).minusMinutes((lengthOfDay*12*60).toLong()+utcOffset)
         val uncorrectedSunset = LocalDateTime.of(date,LocalTime.of(12,0)).plusMinutes((lengthOfDay*12*60).toLong()-utcOffset)
         val sunsetInZ = if (uncorrectedSunset < sunriseInZ) uncorrectedSunset.plusDays(1) else uncorrectedSunset
-        // Log.d("sunrise:", "${sunriseInZ.toDateString()}, ${sunriseInZ.toTimeString()}")
-        // Log.d("sunset:", "${sunsetInZ.toDateString()}, ${sunsetInZ.toTimeString()}")
         return (sunriseInZ..sunsetInZ)
     }
 
@@ -60,7 +56,6 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
      * Checks if it is day at [airport].
      */
     fun itIsDayAt(airport: Airport, time: Instant): Boolean{
-        // Log.d("requested time", "$time")
         return (dayTime(airport.latitude_deg, airport.longitude_deg).contains(LocalDateTime.of(date,time.toLocalTime())))
     }
 
@@ -69,11 +64,9 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
      * @param time: Time in Z as local time
      */
     fun itIsDayAt(airport: Airport, time: LocalTime): Boolean{
-        // Log.d("requested time", "$time")
         return (dayTime(airport.latitude_deg, airport.longitude_deg).contains(LocalDateTime.of(date,time)))
     }
     fun itIsDayAt(lat: Double, lon: Double, time: LocalDateTime): Boolean{
-        // Log.d("requested time", "$time")
         return (dayTime(lat, lon).contains(LocalDateTime.of(date,time.toLocalTime())))
     }
 
@@ -99,16 +92,11 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
 
         if (orig == null || dest == null) return 0
 
-        Log.d("Requested locations", "orig latlon: ${orig.latitude_deg}, ${orig.longitude_deg}")
-        Log.d("Requested locations", "dest latlon: ${dest.latitude_deg}, ${dest.longitude_deg}")
-
         val duration = Duration.between(departureTime, arrivalTime).toMinutes()
-        Log.d("TwilightCalculator", "duration = $duration")
 
         // distance in degrees (*60 gives miles)
         fun getDistance(origLatLon: Pair<Double, Double>, destLatLon:Pair<Double, Double>) = acos(cos((90-origLatLon.first).toRadians())*cos((90-destLatLon.first).toRadians()) + sin((90-origLatLon.first).toRadians()) * sin((90-destLatLon.first).toRadians()) * cos((destLatLon.second-origLatLon.second).toRadians())).toDegrees()
         val distance=getDistance(Pair(orig.latitude_deg, orig.longitude_deg), Pair(dest.latitude_deg, dest.longitude_deg))
-        Log.d ("TwilightCalculator", "Distance is $distance")
 
         // find the vertex of the great circle:
 //        val sinSin = sin(abs(orig.longitude_deg-dest.longitude_deg).toRadians())/sin(distance.toRadians())
@@ -120,8 +108,6 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
         val departureTrack =    if (orig.longitude_deg < dest.longitude_deg) depTrack else 360.0 - depTrack
         // val departureTrack =    if (orig.longitude_deg < dest.longitude_deg) acos((sin(dest.latitude_deg.toRadians())-sin(orig.latitude_deg.toRadians())*cos(distance.toRadians())) / cos(orig.latitude_deg.toRadians()) * cos(distance.toRadians())).toDegrees()
         //                        else 360.0-acos((sin(dest.latitude_deg.toRadians())-sin(orig.latitude_deg.toRadians())*cos(distance.toRadians())) / cos(orig.latitude_deg.toRadians()) * cos(distance.toRadians())).toDegrees()
-
-        Log.d ("TwilightCalculator", "departure track is $departureTrack")
 
         val deltaOrigToVertex = atan(1/(tan(departureTrack.toRadians()) * sin(orig.latitude_deg.toRadians()))).toDegrees()
         val vertexLongitude = orig.longitude_deg + deltaOrigToVertex
@@ -139,20 +125,17 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
             else if (direction == LEFT) (listOf(left) + fiveDegrees.filter{x -> x !in (left..right)}.reversed() + listOf(right))
                 else (listOf(right) + fiveDegrees.filter{x -> x !in (left..right)} + listOf(left))
         // make sure people dont go the long way from 359 to 001; 361 degrees works just fine
-        // Log.d("longitudes passed", "$longitudesPassed")
 
         // a list of potisions (Pair(lat,long))
         val positionsPassed: List<Pair<Double, Double>> = longitudesPassed.map{longitude ->
             val lat = atan(tan(vertexLatitude.toRadians()) * cos((vertexLongitude-longitude).toRadians())).toDegrees()
             Pair(lat, longitude)
         }
-        // Log.d("positions passed", "$positionsPassed")
 
         fun shortStretch(outLatLon: Pair<Double, Double>, inLatLon: Pair<Double, Double>, departTime: LocalDateTime, arrivalTime: LocalDateTime): Int {
             val depTime = departTime.roundToMinutes()
             val arrTime = arrivalTime.roundToMinutes()
             val elapsedTime = Duration.between(depTime, arrTime).toMinutes()
-            Log.d("elapsedTime", "$elapsedTime")
             val latIncrement = (inLatLon.first - outLatLon.first) / elapsedTime
             val longIncrement = (inLatLon.second - outLatLon.second) / elapsedTime
             var day = 0
@@ -160,7 +143,6 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
             for (minute: Long in (0 until elapsedTime)){
                 if (itIsDayAt(outLatLon.first+latIncrement*minute, outLatLon.second+longIncrement*minute, depTime.plusMinutes(minute))) day++ else night++
             }
-            Log.d("TwiLightCalc", "day: $day, night: $night (on this stretch) from $outLatLon to $inLatLon")
             return night
         }
 
@@ -172,7 +154,6 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
 
         //Then times for the positions
         val timePerMile: Double = duration / (totalDist)
-        Log.d("TwilightCalendar", "duration: $duration, distance: $totalDist, timePerMile: $timePerMile")
 
         val posPlusTimes: MutableList<Pair<Pair<Double, Double>, LocalDateTime>> = mutableListOf()
         posPlusTimes.add (positionsPassed[0] to departureTime)
@@ -180,9 +161,7 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
         for (l in 0 until positionsPassed.size -1){
             val d = getDistance(positionsPassed[l], if (positionsPassed[l+1].first == 180.0) -180.0 to positionsPassed[l+1].second else(positionsPassed[l+1])) * 60
             val elapsedSeconds = (d * timePerMile *60).toLong()
-            Log.d("TwilightCalculator", "distance = $d")
             posPlusTimes.add(positionsPassed[l+1] to posPlusTimes[l].second.plusSeconds(elapsedSeconds))
-            Log.d("TwilightCalculator", "added a stretch of $elapsedSeconds seconds!")
         }
         var darkMinutes = 0
 
@@ -194,7 +173,6 @@ class TwilightCalculator(calculateDate: LocalDateTime) { // will know ALL the da
                 posPlusTimes[l].second,
                 posPlusTimes[l + 1].second
             )
-            Log.d("TwilightCalculator", "cumulative night minutes: $darkMinutes")
         }
 
         return darkMinutes

75794efd3f6575fe50881e81e5bce55c901fcfec
Joozd <joozd@ius.org>
2022-01-14 11:20:37 +0100

    Updated copyright

--- app/src/main/java/nl/joozd/logbookapp/ui/adapters/flightsadapter/FlightsAdapter.kt
+++ app/src/main/java/nl/joozd/logbookapp/ui/adapters/flightsadapter/FlightsAdapter.kt
@@ -1,6 +1,6 @@
 /*
  *  JoozdLog Pilot's Logbook
- *  Copyright (c) 2020 Joost Welle
+ *  Copyright (c) 2020-2022 Joost Welle
  *
  *      This program is free software: you can redistribute it and/or modify
  *      it under the terms of the GNU Affero General Public License as

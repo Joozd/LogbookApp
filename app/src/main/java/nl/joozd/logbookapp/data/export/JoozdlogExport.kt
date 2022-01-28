392cc20b8366a61206400fba3b7146fb13cbc01c
joozd <euroshopper@gmail.com>
2022-01-28 14:16:18 +0100

    Updated for use with DispatcherProvider and FlightRepository

--- app/src/main/java/nl/joozd/logbookapp/data/export/JoozdlogExport.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/export/JoozdlogExport.kt
@@ -22,10 +22,10 @@ package nl.joozd.logbookapp.data.export
 import android.net.Uri
 import androidx.core.content.FileProvider
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import nl.joozd.logbookapp.App
-import nl.joozd.logbookapp.data.repository.flightRepository.FlightRepositoryImpl
+import nl.joozd.logbookapp.data.repository.flightRepository.FlightRepository
+import nl.joozd.logbookapp.utils.DispatcherProvider
 import java.io.File
 import java.io.OutputStreamWriter
 import java.util.*
@@ -33,16 +33,17 @@ import java.util.*
 object JoozdlogExport {
     private val context
         get() = App.instance.ctx
-    suspend fun shareCsvExport(fileName: String): Uri = withContext(Dispatchers.Main) {
+    suspend fun shareCsvExport(fileName: String): Uri = withContext(DispatcherProvider.main()) {
         val cachePath = File(context.cacheDir, "files").apply{
             mkdirs()
         }
         val name = if (fileName.uppercase(Locale.ROOT).endsWith(".CSV")) fileName else "$fileName.csv"
         val file = File(cachePath, name)
         OutputStreamWriter(file.outputStream()).use{
-            async(Dispatchers.IO) {
-                it.write(FlightsRepositoryExporter(FlightRepositoryImpl.getInstance()).buildCsvString())
-            }.await()
+            withContext(DispatcherProvider.io()) {
+                @Suppress("BlockingMethodInNonBlockingContext") //blocking call wrapped in DispatcherProvider.io() is OK
+                it.write(FlightsRepositoryExporter(FlightRepository.instance).buildCsvString())
+            }
         }
         FileProvider.getUriForFile(context, "nl.joozd.joozdlog.fileprovider", file)
     }

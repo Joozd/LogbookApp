ec147891f28b629485d45efc8599faa15be32901
joozd <euroshopper@gmail.com>
2022-02-14 09:43:44 +0100

    Added String.hoursAndMinutesStringToInt()

--- app/src/main/java/nl/joozd/logbookapp/model/helpers/minutesToHoursAndMinutesString.kt
+++ app/src/main/java/nl/joozd/logbookapp/model/helpers/minutesToHoursAndMinutesString.kt
@@ -24,3 +24,39 @@ fun Int.minutesToHoursAndMinutesString(): String =
     "${this/60}:${(this%60).toString().padStart(2,'0')}"
 
 fun Long.minutesToHoursAndMinutesString() = this.toInt().minutesToHoursAndMinutesString()
+
+/**
+ * A string that represents hours and minutes gets converted to an Int that represents minutes.
+ * Last two digits are minutes, any before that are hours (so 123456 == 1234:56)
+ * 76 minutes is fine, 104 is 64 minutes.
+ * Users will probably enter something like 330 when they mean 3:30.
+ * Allowed characters are digits and [+- :/.h] (without square brackets)
+ * Empty string is 0, invalid input is null.
+ * Examples:
+ * 23 = 23
+ * 83 = 83; 1:23 = 83; 123 = 83
+ * 123456 = 1234:56 = 74096
+ */
+fun String.hoursAndMinutesStringToInt(): Int? {
+    if (this.isBlank()) return 0
+
+    val hoursAndMinutesSplits = splitIntoHoursAndMinutes(this)
+
+    val hoursAndMinutesInts = try {
+        hoursAndMinutesSplits.map{it.toInt()}
+    } catch (e: NumberFormatException) { return null } // if not only digits left,
+
+    if (hoursAndMinutesInts.isEmpty()) return null // This happens when only characters in "+- :/.h" were in string
+
+    return if (hoursAndMinutesInts.size == 1) {
+        val v = hoursAndMinutesInts.first()
+
+        if (v <= 99) v
+        else v%100 + v/100*60
+    } else hoursAndMinutesInts[0]*60 + hoursAndMinutesInts[1]
+}
+
+private fun splitIntoHoursAndMinutes(hoursAndMinutes: String) =
+    hoursAndMinutes.trim()
+        .split(*"+- :/.h".toCharArray())
+        .filter { it.isNotBlank() } // remove any excess blank spaces that might have been in here

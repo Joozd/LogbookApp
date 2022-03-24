f81cabbf64d305cc69bca3064dc7d4b7bbabbf2f
joozd <euroshopper@gmail.com>
2022-03-24 18:12:21 +0100

    Saving now always returns a result

--- app/src/main/java/nl/joozd/logbookapp/data/importing/ImportedFlightsSaver.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/importing/ImportedFlightsSaver.kt
@@ -22,7 +22,9 @@ package nl.joozd.logbookapp.data.importing
 import nl.joozd.joozdlogimporter.dataclasses.ExtractedCompleteLogbook
 import nl.joozd.joozdlogimporter.dataclasses.ExtractedCompletedFlights
 import nl.joozd.joozdlogimporter.dataclasses.ExtractedPlannedFlights
+import nl.joozd.logbookapp.data.importing.results.SaveCompleteLogbookResult
 import nl.joozd.logbookapp.data.importing.results.SaveCompletedFlightsResult
+import nl.joozd.logbookapp.data.importing.results.SavePlannedFlightsResult
 import nl.joozd.logbookapp.data.repository.airportrepository.AirportRepository
 import nl.joozd.logbookapp.data.repository.flightRepository.FlightRepository
 import nl.joozd.logbookapp.data.repository.flightRepository.FlightRepositoryWithUndo
@@ -31,9 +33,9 @@ interface ImportedFlightsSaver {
     /**
      * Save a complete logbook (from a backup or import)
      */
-    suspend fun save(completeLogbook: ExtractedCompleteLogbook)
+    suspend fun save(completeLogbook: ExtractedCompleteLogbook): SaveCompleteLogbookResult?
     suspend fun save(completedFlights: ExtractedCompletedFlights): SaveCompletedFlightsResult?
-    suspend fun save(plannedFlights: ExtractedPlannedFlights)
+    suspend fun save(plannedFlights: ExtractedPlannedFlights): SavePlannedFlightsResult?
 
     companion object{
         val instance by lazy { make() }
--- app/src/main/java/nl/joozd/logbookapp/data/importing/ImportedFlightsSaverImpl.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/importing/ImportedFlightsSaverImpl.kt
@@ -25,7 +25,9 @@ import nl.joozd.joozdlogimporter.dataclasses.ExtractedCompletedFlights
 import nl.joozd.joozdlogimporter.dataclasses.ExtractedFlights
 import nl.joozd.joozdlogimporter.dataclasses.ExtractedPlannedFlights
 import nl.joozd.joozdlogimporter.enumclasses.AirportIdentFormat
+import nl.joozd.logbookapp.data.importing.results.SaveCompleteLogbookResult
 import nl.joozd.logbookapp.data.importing.results.SaveCompletedFlightsResult
+import nl.joozd.logbookapp.data.importing.results.SavePlannedFlightsResult
 import nl.joozd.logbookapp.data.repository.airportrepository.AirportRepository
 import nl.joozd.logbookapp.data.repository.flightRepository.FlightRepository
 import nl.joozd.logbookapp.data.repository.helpers.iataToIcaoAirports
@@ -48,7 +50,7 @@ class ImportedFlightsSaverImpl(
      * Imported flights will be merged onto flights already in logbook
      * @see mergeFlights
      */
-    override suspend fun save(completeLogbook: ExtractedCompleteLogbook) {
+    override suspend fun save(completeLogbook: ExtractedCompleteLogbook): SaveCompleteLogbookResult {
         val flights = makeFlightsWithIcaoAirportsAndRemoveNamesIfNeeded(completeLogbook)?: emptyList()
         val flightsOnDevice = flightsRepo.getAllFlights()
         // This makes a list of pairs.
@@ -61,6 +63,8 @@ class ImportedFlightsSaverImpl(
         val newFlights = getNonMatchingFlightsExactTimes(flightsOnDevice, flights)
 
         flightsRepo.save(mergedFlights + newFlights)
+
+        return SaveCompleteLogbookResult(true)
     }
 
     /**
@@ -91,9 +95,9 @@ class ImportedFlightsSaverImpl(
      * Will remove all other planned flights in its period.
      * TODO this generates two undo actions, needs fixing
      */
-    override suspend fun save(plannedFlights: ExtractedPlannedFlights) {
-        val flights = prepareFlightsForSaving(plannedFlights) ?: return
-        val plannedFlightsOnDevice = getPlannedFlightsOnDevice(plannedFlights.period ?: return)
+    override suspend fun save(plannedFlights: ExtractedPlannedFlights): SavePlannedFlightsResult {
+        val flights = prepareFlightsForSaving(plannedFlights) ?: return SavePlannedFlightsResult(false)
+        val plannedFlightsOnDevice = getPlannedFlightsOnDevice(plannedFlights.period ?: return SavePlannedFlightsResult(false))
 
         // flights that are an exact match do not get updated or deleted, just stay the way they are.
         // If anything is changed, flights will be merged.
@@ -107,6 +111,8 @@ class ImportedFlightsSaverImpl(
             //Deleting planned flights does not pollute DB as they are not synced to server and thus deleted hard.
         flightsRepo.delete(flightsToDelete)
         flightsRepo.save(mergedFlights + newFlights)
+
+        return SavePlannedFlightsResult(true)
     }
 
     /*

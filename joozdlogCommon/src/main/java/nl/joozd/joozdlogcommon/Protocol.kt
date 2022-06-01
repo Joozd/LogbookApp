92c34f5ac8829488fafe7cdfc9c80e46bddc35ac
joozd <euroshopper@gmail.com>
2022-06-01 20:32:22 +0200

    Added location for datafiles to Protocol

--- joozdlogCommon/src/main/java/nl/joozd/joozdlogcommon/Protocol.kt
+++ joozdlogCommon/src/main/java/nl/joozd/joozdlogcommon/Protocol.kt
@@ -6,4 +6,7 @@ object Protocol {
     const val CURRENTVERSION = VERSION_1
 
     const val KEY_SIZE = 16 // bytes
+
+    const val DATAFILES_METADATA_FILENAME = "datafiles_metadata.json"
+    const val DATAFILES_URL_PREFIX = "https://joozd.nl/joozdlog/"
 }

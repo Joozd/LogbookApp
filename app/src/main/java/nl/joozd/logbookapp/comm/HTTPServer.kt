972af3e41c3d7a61202cc9e6450157dff38ed230
joozd <euroshopper@gmail.com>
2022-06-04 19:44:49 +0200

    Can now inject server address instead of Protocol

--- app/src/main/java/nl/joozd/logbookapp/comm/HTTPServer.kt
+++ app/src/main/java/nl/joozd/logbookapp/comm/HTTPServer.kt
@@ -3,24 +3,23 @@ package nl.joozd.logbookapp.comm
 import android.util.Log
 import kotlinx.coroutines.withContext
 import nl.joozd.joozdlogcommon.DataFilesMetaData
-import nl.joozd.joozdlogcommon.Protocol
+import nl.joozd.joozdlogcommon.comms.Protocol
 import nl.joozd.logbookapp.utils.DispatcherProvider
 import java.io.IOException
 import java.net.URL
 
 // HTTP requests cannot be rejected only fail due to connection problems, so they will either return requested data or null.
 // For testing I can just inject a local url (e.g. file:///c|/temp/)
-class HTTPServer(private val protocol: Protocol = Protocol){
+class HTTPServer(private val server: String = Protocol.SERVER_URL){
     suspend fun getDataFilesMetaData(): DataFilesMetaData? =
         readMetaDataJsonFileToString()?.let { DataFilesMetaData.fromJSON(it) }
-
 
     // Does not check for proper data, null on connection failure.
     private suspend fun readMetaDataJsonFileToString(): String? =
         try {
             withContext(DispatcherProvider.io()) {
                 @Suppress("BlockingMethodInNonBlockingContext")
-                URL(protocol.DATAFILES_URL_PREFIX + protocol.DATAFILES_METADATA_FILENAME).readText()
+                URL(server + Protocol.DATAFILES_URL_PREFIX + Protocol.DATAFILES_METADATA_FILENAME).readText()
             }
         } catch (ioe: IOException){
             Log.w("readMetaDataJsonString", "IO error when trying to read url")

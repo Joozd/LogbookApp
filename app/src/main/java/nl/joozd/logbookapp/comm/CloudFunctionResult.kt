9e7cb4e138de6efcad4b562a56350ebb584bf3f0
joozd <euroshopper@gmail.com>
2022-06-08 10:09:11 +0200

    moved correspondingServerFunctionResult() into class

--- app/src/main/java/nl/joozd/logbookapp/comm/CloudFunctionResult.kt
+++ app/src/main/java/nl/joozd/logbookapp/comm/CloudFunctionResult.kt
@@ -5,8 +5,16 @@ enum class CloudFunctionResult {
 
     CONNECTION_ERROR,   // could not establish a conenction for the wanted task
 
-    SERVER_REFUSED      // server refused to complete wanted task (e.g. because of bad login data)
+    SERVER_REFUSED;     // server refused to complete wanted task (e.g. because of bad login data)
                         // Function returning this CLoudFunctionResult must handle the cause of this
                         // result, (e.g. set flag for bad login data so TaskDispatcher can do some-
                         // thing about it)
+
+    fun correspondingServerFunctionResult(): ServerFunctionResult = when(this){
+        OK -> ServerFunctionResult.SUCCESS
+        CONNECTION_ERROR -> ServerFunctionResult.RETRY
+        SERVER_REFUSED -> ServerFunctionResult.FAILURE
+    }
+
+    fun isOK() = this == OK
 }

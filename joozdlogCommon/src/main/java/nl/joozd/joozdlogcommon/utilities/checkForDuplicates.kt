b7cdf00febd2e1d33e03329811907a1ea8494f39
joozd <euroshopper@gmail.com>
2022-07-06 01:21:52 +0200

    replaces .equals with ==

--- joozdlogCommon/src/main/java/nl/joozd/joozdlogcommon/utilities/checkForDuplicates.kt
+++ joozdlogCommon/src/main/java/nl/joozd/joozdlogcommon/utilities/checkForDuplicates.kt
@@ -7,6 +7,8 @@ import nl.joozd.joozdlogcommon.BasicFlight
  * These duplicates should be deleted hard, both locally and on server.
  * Use this function to get the flights that need deleting.
  * @return a list of BasicFlights that need deleting. These are the duplicate entries with the highest IDs.
+ * NOTE this might take some time on bigger datasets, so it is suggested to run this in Dispatchers.Default
+ * TODO make separate Cloud function for removing duplicates from DB as this turns out to be rather expensive to do every time.
  */
 fun checkForDuplicates(flights: List<BasicFlight>): List<BasicFlight> {
     val flightsToKeep = ArrayList<BasicFlight>(flights.size)
@@ -21,4 +23,4 @@ fun checkForDuplicates(flights: List<BasicFlight>): List<BasicFlight> {
 
 // A flight is a duplicate if everything except flightID, timestamp and changed are the same.
 private fun BasicFlight.isDuplicateOf(other: BasicFlight) =
-    this.copy(flightID = 0, changed = false, timeStamp = 0).equals(other.copy(flightID = 0, changed = false, timeStamp = 0))
+    this.copy(flightID = 0, changed = false, timeStamp = 0) == other.copy(flightID = 0, changed = false, timeStamp = 0)

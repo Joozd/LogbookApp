e45ddb6af9478305840466c9ea89409a3e5f0ed2
joozd <euroshopper@gmail.com>
2022-08-27 11:44:52 +0200

    Cleaning up dead code

--- app/src/main/java/nl/joozd/logbookapp/model/feedbackEvents/FeedbackEvents.kt
+++ app/src/main/java/nl/joozd/logbookapp/model/feedbackEvents/FeedbackEvents.kt
@@ -22,67 +22,12 @@ package nl.joozd.logbookapp.model.feedbackEvents
 object FeedbackEvents {
     interface Event
 
-    enum class GeneralEvents:
-    Event{
-        DONE,
-        NOT_IMPLEMENTED,
-        ERROR,
-        OK
-    }
-
-    enum class MainActivityEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        SHOW_ABOUT_DIALOG,
-        FLIGHT_NOT_FOUND,
-        TRYING_TO_DELETE_COMPLETED_FLIGHT,
-        TRYING_TO_DELETE_CALENDAR_FLIGHT,
-        DELETED_FLIGHT,
-        OPEN_SEARCH_FIELD,
-        CLOSE_SEARCH_FIELD,
-        CALENDAR_SYNC_PAUSED,
-        BACKUP_NEEDED,
-        EMAIL_VERIFIED,
-        LOGGED_IN,
-        LOGIN_DELAYED_DUE_NO_SERVER,
-        DONE,
-        ERROR
-    }
-
-    enum class PdfParserActivityEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        IMPORTING_LOGBOOK,
-        ROSTER_SUCCESSFULLY_ADDED,
-        CHRONO_SUCCESSFULLY_ADDED,
-        CHRONO_CONFLICTS_FOUND_AND_NOT_ALL_PLANNED,
-        CHRONO_CONFLICTS_FOUND,
-        CHRONO_DOES_NOT_FILL_ALL_PLANNED,
-        CALENDAR_SYNC_ENABLED,
-        CALENDAR_SYNC_PAUSED,
-        FILE_NOT_FOUND,
-        UNSUPPORTED_FILE,
-        NOT_A_KNOWN_ROSTER,
-        NOT_A_KNOWN_LOGBOOK,
-        PARSING_COMPLETE_LOGBOOK,
-        ERROR
-    }
-
     enum class MakePdfActivityEvents: Event{
         BUILDING_LOGBOOK,
         LOGBOOK_READY,
         WRITING,
         FILE_CREATED
 
-    }
-
-    enum class SettingsActivityEvents:
-        Event {
-        CALENDAR_DIALOG_NEEDED,
-        LOGIN_LINK_COPIED,
-        NOT_LOGGED_IN,
-        WANT_TO_CREATE_NEW_ACCOUNT_QMK,
-        SIGNED_OUT
     }
 
     enum class BalanceForwardActivityEvents:
@@ -106,108 +51,10 @@ object FeedbackEvents {
         FINISHED
     }
 
-    enum class CreateNewUserActivityEvents: Event{
-        USER_EXISTS,
-        USERNAME_TOO_SHORT, // ie. empty username provided
-        NO_INTERNET,
-        WAITING_FOR_SERVER,
-        SERVER_NOT_RESPONDING,
-        FINISHED
-    }
-
-    enum class ChangePasswordEvents: Event {
-        FINISHED,
-        NOT_IMPLEMENTED,
-        LOGIN_LINK_COPIED,
-        NO_INTERNET,
-        NOT_LOGGED_IN,
-        WAITING_FOR_SERVER,
-        SERVER_NOT_RESPONDING,
-        LOGIN_INCORRECT
-    }
-
-    enum class NewUserActivityEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        UPDATE_NAVBAR,
-        BAD_EMAIL,
-        EMAILS_DO_NOT_MATCH,
-        SHOW_TERMS_DIALOG,
-        CLEAR_PAGE,
-        NEXT_PAGE,
-        FINISHED
-    }
-
-
     enum class BalanceForwardDialogEvents:
         Event {
         UPDATE_FIELDS,
         NUMBER_PARSE_ERROR,
         CLOSE_DIALOG
     }
-
-    enum class EditFlightFragmentEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        SAVING,
-        EDIT_FLIGHT_CALENDAR_CONFLICT,
-        CLOSE_EDIT_FLIGHT_FRAGMENT,
-        INVALID_REG_TYPE_STRING,
-        AIRPORT_NOT_FOUND_FOR_LANDINGS,
-        INVALID_TIME_STRING,                // flight not updated due bad time string
-        INVALID_SIM_TIME_STRING,            // Flight updated with 0 sim time due bad string format
-        REGISTRATION_NOT_FOUND,             // registration has no match in local DB or consensus
-        REGISTRATION_HAS_MULTIPLE_CONSENSUS,// registration has multiple hits in consensus
-                                            // extraData = List of found registrations in consensus
-        AIRPORT_NOT_FOUND,
-        AIRCRAFT_NOT_FOUND,
-        ERROR
-
-    }
-
-    enum class AirportPickerEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        CUSTOM_AIRPORT_NOT_EDITED,
-        ORIG_OR_DEST_NOT_SELECTED
-    }
-
-    enum class NamesDialogEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        NAME1_OR_NAME2_NOT_SELECTED
-    }
-
-    enum class TimePickerEvents:
-        Event {
-        NOT_IMPLEMENTED,
-        FLIGHT_IS_NULL,
-        INVALID_TOTAL_TIME,
-        INVALID_NIGHT_TIME,
-        NIGHT_TIME_GREATER_THAN_DURATION,
-        TOTAL_TIME_GREATER_THAN_DURATION,
-        INVALID_IFR_TIME,
-        IFR_TIME_GREATER_THAN_DURATION
-    }
-
-    enum class CalendarSyncDialogEvents: Event {
-        NO_ICAL_LINK_FOUND,
-        ERROR,
-        DONE
-    }
-
-    enum class FlightEditorOpenOrClosed:
-        Event {
-        OPENED,
-        CLOSED
-    }
-
-    enum class GenericEvents:
-        Event {
-        EVENT_1,
-        EVENT_2,
-        EVENT_3
-    }
-
-
 }

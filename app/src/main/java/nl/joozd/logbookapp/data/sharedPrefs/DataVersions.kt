0a06bc40c959f59c2bf04b702bc768bff851108b
joozd <euroshopper@gmail.com>
2022-09-06 09:38:30 +0200

    cleaning up empty lines

--- app/src/main/java/nl/joozd/logbookapp/data/sharedPrefs/DataVersions.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/sharedPrefs/DataVersions.kt
@@ -8,12 +8,9 @@ object DataVersions: JoozdLogPreferences() {
     private const val AIRCRAFT_FORCED_TYPES_VERSION_KEY = "AIRCRAFT_FORCED_TYPES_VERSION_KEY"
     private const val AIRPORTS_VERSION_KEY = "AIRPORTS_VERSION_KEY"
 
-
     val mostRecentDataFilesSyncEpochSecond by JoozdlogSharedPreferenceDelegate(MOST_RECENT_SYNC_EPOCH_SECOND, -1L)
 
     val aircraftTypesVersion by JoozdlogSharedPreferenceDelegate(AIRCRAFT_TYPES_VERSION_KEY, -1)
     val aircraftForcedTypesVersion by JoozdlogSharedPreferenceDelegate(AIRCRAFT_FORCED_TYPES_VERSION_KEY, -1)
     val airportsVersion by JoozdlogSharedPreferenceDelegate(AIRPORTS_VERSION_KEY, -1)
-
-
 }

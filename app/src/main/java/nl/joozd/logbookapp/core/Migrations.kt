9c88c8d52911f37666738b3ba34ce7920ce94e46
joozd <euroshopper@gmail.com>
2022-10-20 16:34:50 +0200

    Migrating from 0 to 1 now remembers that it did that (updates Prefs.configuredVersion)

--- app/src/main/java/nl/joozd/logbookapp/core/Migrations.kt
+++ app/src/main/java/nl/joozd/logbookapp/core/Migrations.kt
@@ -20,12 +20,15 @@ class Migrations(private val prefs: Prefs = Prefs) {
 
     private val migrations: Map<Int, suspend () -> Unit> = mapOf(
         0 to this::migrate0to1
+    // 1 to this::migrate1to2 // etc
     )
 
     /*
         Migration takes care of replacing any old email data (login name based) to updated version (id based)
      */
     private suspend fun migrate0to1(){
+        require(prefs.configuredVersion() == 0) { "Cannot migrate from 0 if current version is not 0" }
         EmailCenter().migrateEmailDataIfNeeded()
+        prefs.configuredVersion(1)
     }
 }

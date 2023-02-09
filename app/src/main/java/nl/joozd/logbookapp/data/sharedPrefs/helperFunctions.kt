9392d04d06e09eabdb8a02b72d28ec7a6fc5f451
joozd <euroshopper@gmail.com>
2023-02-09 13:31:23 +0100

    Added doc

--- app/src/main/java/nl/joozd/logbookapp/data/sharedPrefs/helperFunctions.kt
+++ app/src/main/java/nl/joozd/logbookapp/data/sharedPrefs/helperFunctions.kt
@@ -2,8 +2,10 @@ package nl.joozd.logbookapp.data.sharedPrefs
 
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import nl.joozd.logbookapp.data.sharedPrefs.JoozdlogSharedPreferenceDelegate
 
+/**
+ * Toggle the value of a Pref<Boolean> from true to false or vice versa.
+ */
 fun JoozdlogSharedPreferenceDelegate.Pref<Boolean>.toggle(){
     MainScope().launch {
         setValue(!invoke())
